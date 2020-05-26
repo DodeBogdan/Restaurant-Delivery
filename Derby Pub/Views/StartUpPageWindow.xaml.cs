a7d661e938a07cf7d8323403fdd31c775d814747
Bogdan Gheorghe <dodebogdan@yahoo.com>
2020-05-26 12:20:37 +0300

    Add project files.

---
+++ Derby Pub/Views/StartUpPageWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Derby_Pub
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class StartUpPageWindow : Window
+    {
+        public StartUpPageWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+
+        private void NoAccountButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ClientButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
